19f5a04e5e3e5ff5d07e128497a5c6694ac45ea9
Bob Scott <bobscott78@gmail.com>
2024-12-04 13:25:42 +0000

    More than one line

--- day1/part-1/getDistance.test.ts
+++ day1/part-1/getDistance.test.ts
@@ -1,17 +1,36 @@
 import { describe, it, expect } from 'vitest';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as readline from 'readline';
 
 describe('first file with one line', () => {
-    it('should return difference between the numbers', () => {
-        expect(getDistance("./puzzle-input-1-line.txt")).toBe(1);
-    });
+  it('should return difference between the numbers', async () => {
+    expect(await getDistance("./puzzle-input-1-line.txt")).toBe(1);
+  });
 });
 
-function getDistance(filename: string): number {
-  const filePath = path.join(__dirname, filename);
-  const fileContent = fs.readFileSync(filePath, 'utf-8');
-  const [num1, num2] = fileContent.split(/\s+/).map(Number);
+describe('two nicely sorted lines', () => {
+  it('should return difference between the numbers', async () => {
+    expect(await getDistance("./puzzle-input-2-sorted-lines.txt")).toBe(3);
+  });
+});
 
-  return Math.abs(num1 - num2);
+
+async function getDistance(filename: string): Promise<number> {
+  const filePath = path.join(__dirname, filename);
+  let runningTotal = 0;
+  
+  const fileStream = fs.createReadStream(filePath);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) {
+    const [num1, num2] = line.split(/\s+/).map(Number);
+    runningTotal += Math.abs(num1 - num2);
+  }
+
+  return runningTotal;
 }
