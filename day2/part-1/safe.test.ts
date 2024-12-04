20516512906bd9bdd80eec8d8ea00a9e41eaedae
Bob Scott <bobscott78@gmail.com>
2024-12-04 16:06:53 +0000

    Always increasing

--- day2/part-1/safe.test.ts
+++ day2/part-1/safe.test.ts
@@ -1,18 +1,20 @@
 import { describe, expect, it } from "vitest";
-import * as fs from 'fs';
-import * as path from 'path';
-import * as readline from 'readline';
-
 
 describe('safe', () => {
-  it('big increases are unsafe', () => {
+  it('is unsafe for big increases', () => {
     const report = "1 2 6 8 9";
     const isSafe = safe(report);
     expect(isSafe).toBe(false);
   });
 
-  it('big decreases are unsafe', () => {
+  it('is unsafe for big decreases', () => {
     const report = "9 8 6 2 1";
+    const isSafe = safe(report);
+    expect(isSafe).toBe(false);
+  });
+
+  it('is unsafe if increases then decreases', () => {
+    const report = "1 3 2 4 5";
     const isSafe = safe(report);
     expect(isSafe).toBe(false);
   });
@@ -23,10 +25,14 @@ function safe(report: string) {
   let index = 0;
   let safe = true;
 
+  const increasing = numbers[0] < numbers[1];
   while (safe && index < numbers.length - 2) {
     const current = numbers[index];
     const next = numbers[index + 1];
     safe = Math.abs(current - next) <= 3;
+    if (increasing) {
+      safe = safe && next > current;
+    } 
     index++;
   }
   return safe;
