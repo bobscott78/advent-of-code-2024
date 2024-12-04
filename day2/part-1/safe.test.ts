fcc000b502c1541a2d8c18bfec7d41b4f3d7f8e9
Bob Scott <bobscott78@gmail.com>
2024-12-04 16:22:56 +0000

    Read reports from file

--- day2/part-1/safe.test.ts
+++ day2/part-1/safe.test.ts
@@ -1,4 +1,7 @@
 import { describe, expect, it } from "vitest";
+import * as fs from 'fs';
+import * as path from 'path';
+import * as readline from 'readline';
 
 describe('safe', () => {
   it('is unsafe for big increases', () => {
@@ -18,7 +21,32 @@ describe('safe', () => {
     const isSafe = safe(report);
     expect(isSafe).toBe(false);
   });
+
+  it('is unsafe if decreases then increases', () => {
+    const report = "5 4 2 3 1";
+    const isSafe = safe(report);
+    expect(isSafe).toBe(false);
+  });
+
+  it('is unsafe if 2 consecutive', () => {
+    const report = "1 2 4 4 5";
+    const isSafe = safe(report);
+    expect(isSafe).toBe(false);
+  });
+
+  it('is unsafe if 2 consecutive at end', () => {
+    const report = "6 5 4 2 2";
+    const isSafe = safe(report);
+    expect(isSafe).toBe(false);
+  });
 });
+
+describe('safe from file', () => {
+  it('counts safe reports', async () => {
+    expect(await safeFromFile('./puzzle-input-sample.txt')).toBe(2);
+  });
+});
+
 
 function safe(report: string) {
   const numbers = report.split(' ').map(Number);
@@ -26,15 +54,37 @@ function safe(report: string) {
   let safe = true;
 
   const increasing = numbers[0] < numbers[1];
-  while (safe && index < numbers.length - 2) {
+  while (safe && index < numbers.length - 1) {
     const current = numbers[index];
     const next = numbers[index + 1];
     safe = Math.abs(current - next) <= 3;
     if (increasing) {
       safe = safe && next > current;
-    } 
+    }
+    else {
+      safe = safe && next < current;
+    }
     index++;
   }
   return safe;
+}
+
+async function safeFromFile(filename: string): Promise<number> {
+  const filePath = path.join(__dirname, filename);
+  
+  const fileStream = fs.createReadStream(filePath);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  let count = 0;
+  for await (const line of rl) {
+    if (safe(line)) {
+      count++;
+    }
+  }
+  return count;
 }
 
