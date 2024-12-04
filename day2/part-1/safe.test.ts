cad62d4f0a6f4ea7226bbcee0f9229d27c62dc7a
Bob Scott <bobscott78@gmail.com>
2024-12-04 15:53:39 +0000

    Big increases

---
+++ day2/part-1/safe.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from "vitest";
+import * as fs from 'fs';
+import * as path from 'path';
+import * as readline from 'readline';
+
+
+describe('safe', () => {
+  it('big increases are unsafe', () => {
+    const report = "1 2 6 8 9";
+    const isSafe = safe(report);
+    expect(isSafe).toBe(false);
+  });
+});
+
+function safe(report: string) {
+  const numbers = report.split(' ').map(Number);
+  let index = 0;
+  let safe = true;
+  while (safe && index < numbers.length - 2) {
+    const current = numbers[index];
+    const next = numbers[index + 1];
+    safe = next <= current + 3;
+    index++;
+  }
+  return safe;
+}
+
