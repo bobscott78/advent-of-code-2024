75ddbdd85d144f8c6428314b1b7fc2ddddc1b85c
Bob Scott <bobscott78@gmail.com>
2024-12-04 14:03:03 +0000

    Score sample file

--- day1/part-2/similarityScore.test.ts
+++ day1/part-2/similarityScore.test.ts
@@ -7,6 +7,14 @@ describe('similarity score', () => {
   it('should return score for 2 lines', async () => {
     expect(await similarityScore("./puzzle-input-2-lines.txt")).toBe(6);
   });
+
+  it('should return score for sample lists', async () => {
+    expect(await similarityScore("./puzzle-input-sample.txt")).toBe(31);
+  });
+
+  it.skip('should return score for puzzle input', async () => {
+    expect(await similarityScore("../part-1/puzzle-input.txt")).toBe(0);
+  });
 });
 
 async function similarityScore(filename: string): Promise<number> {
@@ -23,9 +31,7 @@ async function similarityScore(filename: string): Promise<number> {
 
   for await (const line of rl) {
     const [num1, num2] = line.split(/\s+/).map(Number);
-    if (!first.includes(num1)) {
-      first.push(num1);
-    }
+    first.push(num1);
     second.set(num2, (second.get(num2) || 0) + 1);
   }
 
