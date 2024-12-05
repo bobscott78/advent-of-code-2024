6713bbb2f4e50f6cfe7b4359e92e77373d8dd9b2
Bob Scott <bobscott78@gmail.com>
2024-12-05 09:30:53 +0000

    Day 3: full sample

--- day3/part-1/sumMultiplications.test.ts
+++ day3/part-1/sumMultiplications.test.ts
@@ -4,15 +4,25 @@ describe('sumMultiplications', () => {
   it('extracts sungle instruction', () => {
     expect(sumMultiplications("xmul(2,4)%")).toBe(8);
   });
+
+  it('extracts multiple instructions', () => {
+    expect(sumMultiplications("xmul(2,4)%mul(3,50)%")).toBe(158);
+  });
+
+  it('sums the full sample', () => {
+    expect(sumMultiplications("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")).toBe(161);
+  });
 });
 
-function sumMultiplications(instructions: string): number {
-  const regex = /mul\((\d+),(\d+)\)/;
-  const match = instructions.match(regex);
-  
-  if (match) {
-      return parseInt(match[1], 10) * parseInt(match[2], 10);
+function sumMultiplications(instructions: string): number {  
+  const regex = /mul\((\d+),(\d+)\)/g;
+  let result = 0;
+  let match;
+
+  while ((match = regex.exec(instructions)) !== null) {
+    result += parseInt(match[1], 10) * parseInt(match[2], 10);
   }
-  return 0
+
+  return result;
 }
 
