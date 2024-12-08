b90270699e5f72bb5f09efac28ca701a3e86f1cb
Bob Scott <bobscott78@gmail.com>
2024-12-08 15:09:06 +0000

    Day7: multiplying satisfies equation

--- day7/part-1/calibrationResult.test.ts
+++ day7/part-1/calibrationResult.test.ts
@@ -1,7 +1,15 @@
 import { describe, expect, it } from "vitest";
 
 describe('calibration result', () => {
-  it('should find the correct result', () => {
-    expect(true).toBe(true);
+  it('first sample could be true', () => {
+
+    expect(couldBeTrue('190: 10 19')).toBe(true);
   });
 });
+
+function couldBeTrue(equation: string): boolean {
+  const [left, right] = equation.split(':');
+  const testValue = Number(left);
+  const operands = right.split(' ').map(Number);
+  return testValue === operands[0] * operands[1];
+}
